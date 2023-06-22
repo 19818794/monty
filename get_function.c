4e50fa30b1fa2c943474d5c50acbcc10a95069d0
19818794 <jory2ray@gmail.com>
2023-06-22 10:51:12 -0700

    nop

--- get_function.c
+++ get_function.c
@@ -17,6 +17,7 @@ void (*get_function(char *str))(stack_t **stack, unsigned int line_number)
 		{"pop", _pop},
 		{"swap", _swap},
 		{"add", _add},
+		{"nop", _nop},
 		{NULL, NULL},
 	};
 
--- opcodes_functions2.c
+++ opcodes_functions2.c
@@ -22,3 +22,15 @@ void _add(stack_t **stack, unsigned int line_number)
 	_pop(stack, line_number);
 	(*stack)->n = sum;
 }
+/**
+ * _nop - doesn’t do anything.
+ * @stack: a list for the monty stack.
+ * @line_number: the line number on which the opcode appears.
+ *
+ * Return: void.
+ */
+void _nop(stack_t **stack __attribute__ ((unused)),
+	unsigned int line_number __attribute__ ((unused)))
+{
+	;
+}
